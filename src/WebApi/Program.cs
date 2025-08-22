9317f2e11799a09a64685a2df836fad4f0b24c84
Tomasz Plewniak <t_plewniak@yahoo.com>
2025-08-22 15:45:01 +0200

    Register db context.

--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -1,6 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using WebApi.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
