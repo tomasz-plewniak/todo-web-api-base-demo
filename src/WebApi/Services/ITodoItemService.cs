564b10a492942840e1549ecb7094d657acbd1e98
Tomasz Plewniak <t_plewniak@yahoo.com>
2025-08-27 17:55:09 +0200

    Add service.

---
+++ src/WebApi/Services/ITodoItemService.cs
@@ -0,0 +1,16 @@
+﻿using WebApi.Models;
+
+namespace WebApi.Services;
+
+public interface ITodoItemService
+{
+    public Task<IEnumerable<TodoItem>> GetTodoItemsAsync();
+    
+    public Task<TodoItem?> GetTodoItemAsync(Guid id);
+    
+    public Task<TodoItem> CreateTodoItemAsync(CreateTodoItem createTodoItem);
+    
+    public Task<TodoItem> UpdateTodoItemAsync(Guid id, UpdateTodoItem updateTodoItem);
+    
+    public Task DeleteTodoItemAsync(Guid id);
+}
---
+++ src/WebApi/Services/TodoItemService.cs
@@ -0,0 +1,31 @@
+﻿using WebApi.Models;
+
+namespace WebApi.Services;
+
+public class TodoItemService : ITodoItemService
+{
+    public Task<IEnumerable<TodoItem>> GetTodoItemsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TodoItem?> GetTodoItemAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TodoItem> CreateTodoItemAsync(CreateTodoItem createTodoItem)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<TodoItem> UpdateTodoItemAsync(Guid id, UpdateTodoItem updateTodoItem)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task DeleteTodoItemAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
