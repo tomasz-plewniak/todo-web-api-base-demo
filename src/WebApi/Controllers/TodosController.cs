17617d389cd4bd6c9279b686025a7096eef0344b
Tomasz Plewniak <t_plewniak@yahoo.com>
2025-08-27 17:50:11 +0200

    Add base endpoints.

--- src/WebApi/Controllers/TodosController.cs
+++ src/WebApi/Controllers/TodosController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
 
 namespace WebApi.Controllers;
 
@@ -6,4 +7,33 @@ namespace WebApi.Controllers;
 [Route("[controller]")]
 public class TodosController : ControllerBase
 {
+    [HttpGet]
+    public async Task<IActionResult>  GetTodoItemsAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetTodoItemAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateTodoItemAsync(CreateTodoItem createTodoItem)
+    {
+        throw new NotImplementedException();
+    }
+    
+    [HttpPatch("{id}")]
+    public async Task<IActionResult> UpdateTodoItemAsync(Guid id, UpdateTodoItem updateTodoItem)
+    {
+        throw new NotImplementedException();   
+    }
+    
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteTodoItemAsync(Guid id)
+    {
+        throw new NotImplementedException();  
+    }
 }
