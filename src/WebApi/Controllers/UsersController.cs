6bd9cb433d417fd61436cbaa51236f5bf85131ab
Tomasz Plewniak <t_plewniak@yahoo.com>
2025-08-29 20:00:12 +0200

    Add new endpoint.

---
+++ src/WebApi/Controllers/UsersController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models;
+using WebApi.Services;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult>  GetUsersAsync(CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<IActionResult> GetUserAsync(
+        [FromRoute] Guid id,
+        CancellationToken cancellationToken = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateUserAsync(
+        // Add here create.
+        )
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPatch("{id}")]
+    public async Task<IActionResult> UpdateUserAsync(
+        [FromRoute] Guid id
+        // Add here update model)
+        )
+    {
+        throw new NotImplementedException();   
+    }
+    
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUserAsync(
+        [FromRoute] Guid id)
+    {
+        throw new NotImplementedException();  
+    }
+}
