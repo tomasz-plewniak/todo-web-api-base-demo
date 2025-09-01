8bc6f281ad0499c1da941b962d6aa95498ca90f9
Tomasz Plewniak <t_plewniak@yahoo.com>
2025-09-01 15:13:17 +0200

    Add validation for create user model.

--- src/WebApi/Models/CreateUser.cs
+++ src/WebApi/Models/CreateUser.cs
@@ -1,5 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace WebApi.Models;
 
 public record CreateUser(
+    [Required]
+    [Length(1, 50)]
     string UserName,
+    [Required]
+    [EmailAddress]
     string Email);
