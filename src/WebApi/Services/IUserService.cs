8b80d69fabff3ad0f93d39bfb70797794226a1fa
Tomasz Plewniak <t_plewniak@yahoo.com>
2025-09-03 20:07:05 +0200

    Implement delete user.

--- src/WebApi/Services/IUserService.cs
+++ src/WebApi/Services/IUserService.cs
@@ -11,4 +11,6 @@ public interface IUserService
     Task<User> CreateUserAsync(CreateUser createUser);
     
     Task UpdateUserAsync(User updatedUser);
+    
+    Task DeleteUserAsync(User user);
 }
