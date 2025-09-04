0ba18b68fb94221a427d60876b460647de42fd69
Tomasz Plewniak <t_plewniak@yahoo.com>
2025-09-04 19:18:31 +0200

    Update model.

--- src/WebApi/Models/CreateTodoItem.cs
+++ src/WebApi/Models/CreateTodoItem.cs
@@ -1,6 +1,19 @@
-﻿namespace WebApi.Models;
+﻿using System.ComponentModel.DataAnnotations;
 
-public class CreateTodoItem
+namespace WebApi.Models;
+
+public record CreateTodoItem
 {
+    [MaxLength(100)]
+    public required string Title { get; init; }
+
+    [MaxLength(1000)]
+    public string? Description { get; init; }
     
+    public DateTime? DueDate { get; init; }
+
+    public Priority? Priority { get; init; }
+
+    [Required]
+    public Guid UserId { get; init; }
 }
